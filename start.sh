bbfb949a9b07751306349d619b599cccdf383a22
anuragperiwal0 <116804267+anuragperiwal0@users.noreply.github.com>
2023-08-02 15:24:57 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/anuragperiwal0/cheking.git /cheking
+  git clone https://github.com/AM-ROBOTS/ApnaTube.git /ApnaTube
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /cheking
+  git clone $UPSTREAM_REPO /ApnaTube
 fi
-cd /cheking
+cd /ApnaTube
 pip3 install -U -r requirements.txt
 echo "Starting ApnaTube...."
 python3 bot.py
